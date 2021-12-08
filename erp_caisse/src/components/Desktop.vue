5ceeb0dca241b1e63720b13e01ca10a5fc0b4ece
gabriel.ludel <gabriel.ludel@etu.unistra.fr>
2021-12-08 19:17:10 +0100

    version mobile + desktop

--- erp_caisse/src/components/Desktop.vue
+++ erp_caisse/src/components/Desktop.vue
@@ -1,68 +1,206 @@
 <template>
-	<v-row> 
-		<v-col cols ="1">
-		<v-navigation-drawer
+	<div v-resize="onResize">
+	<v-navigation-drawer
+			v-model="drawer"
+			absolute
 			permanent
 		>
-			<v-list>
-				<v-list-item-group  v-model="selectedSet">
-					<v-list-item 
+			<v-list nav class="overflow-y-auto" :max-height=computedHeight>
+				<v-list-item-group v-model="selectedSet">
+					<v-list-item
 						v-for="item in products"
 						v-bind:key="item.name"
-						@click="displayShop = true;selectedPage = null;"
-					>
-						<v-list-item-icon>
-							<v-icon large>{{ item.icon }}</v-icon>
-						</v-list-item-icon>
-						<v-list-item-content></v-list-item-content>
+						@click="displaySet = true">
+						<v-list-item-icon><v-icon>{{item.icon}}</v-icon></v-list-item-icon>
+						<v-list-item-title>{{item.name}}</v-list-item-title>
 					</v-list-item>
-				</v-list-item-group>
-			</v-list><v-divider></v-divider>
-			<v-list>
-				<v-list-item-group v-model="selectedPage">
+					<v-divider></v-divider>
 					<v-list-item 
 						v-for="item in menus"
 						v-bind:key="item.name"
-						@click="setMenu"
+						@click="displaySet = false"
 					>
 						<v-list-item-icon>
-							<v-icon large>{{item.icon}}</v-icon>
+							<v-icon >{{item.icon}}</v-icon>
 						</v-list-item-icon>
-						<v-list-item-content></v-list-item-content>
+						<v-list-item-title>{{item.name}}</v-list-item-title>
+					</v-list-item>
+
+				</v-list-item-group>
+			</v-list>
+		</v-navigation-drawer>
+
+		<v-row class="ml-10 mr-5">
+			<v-col cols="2"></v-col>
+			<v-col cols="8">
+				<div v-if="displaySet" >
+					<v-list class="overflow-y-auto" :max-height=computedHeight >
+						<v-list-item>
+						<v-row class="fill-height pa-5">
+							<v-col cols="4" 
+								class="pa-0"
+								v-for="item in set.articles"
+								v-bind:key="item.name"
+							>
+								<v-card 
+									outlined
+									exact
+									height="100%">
+									<v-card-title> {{item.name}}</v-card-title>
+									<v-card-text class="pl-5 font-light-black"> {{item.prix}}€</v-card-text>
+									<v-card-actions>
+										<v-btn 
+											block
+											@click="addArt(item)"> ADD </v-btn>
+									</v-card-actions>
+								</v-card>
+							</v-col>
+						</v-row>
+						</v-list-item>
+					</v-list>
+				</div>
+				<!-- *************************** USERS LIST ******************* -->
+				<div v-else-if="selectedSet == products.length + 0" class="ma-5 mr-10">  
+					<v-card> 
+						<v-card-title> Users </v-card-title>
+						<v-text-field
+							class="pa-5 pt-0"
+							label="Search username"
+							v-model="searchInput"
+							hide-details="auto"
+						></v-text-field>
+					</v-card>
+					<v-list class="overflow-y-auto" :max-height=computedHeight >
+						<v-list-item>
+							<v-expansion-panels >
+								<v-expansion-panel
+									v-for="item in users"
+									v-bind:key="item.id"
+								>
+									<v-expansion-panel-header>
+										{{item.username}}
+									</v-expansion-panel-header>
+									<v-expansion-panel-content>
+										<span class="font-weight-black"> Name : </span>{{item.username}} <br/>
+										<span class="font-weight-black"> Id : </span>{{item.id}} <br/>
+										<span class="font-weight-black"> Credit : </span>{{item.credit}} <br/>
+										<span class="font-weight-black"> {{item.username}}'s transactions historic : </span><br/>
+										
+										<v-data-table
+											:headers="headers"
+											:items="item.historic">
+										</v-data-table>
+									</v-expansion-panel-content>
+								</v-expansion-panel>
+							</v-expansion-panels>
+						</v-list-item>
+					</v-list>
+				</div>
+
+		<!-- *********************** ADD MONEY ****************************** -->
+				<div v-else-if="selectedSet == products.length + 1"
+					class="ma-10 ml-5"
+				>
+					<v-card>
+						<v-card-title>Add credit</v-card-title>
+						<v-card-text>
+							<v-text-field 
+								label="User ID"
+							></v-text-field>
+							<v-text-field 
+								label="Amount"
+							></v-text-field>
+							<v-text-field
+								label="Administrator password"
+								:type="show ? 'text' : 'password'"
+								:append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+								@click:append="show = !show"
+							></v-text-field>
+							<v-btn block text color="primary"> Valid </v-btn>
+						</v-card-text>
+					</v-card>
+				</div>
+			</v-col>
+		</v-row>
+
+
+		<v-navigation-drawer
+			absolute
+			permanent
+			right
+		>
+			<v-card height="50px" elevation="0" flat><v-card-title></v-card-title></v-card>
+
+			<v-list class="overflow-y-auto" :max-height=computedHeight>
+				<v-list-item-group>
+					<v-list-item 
+						v-for="item in userCart.cart"
+						v-bind:key="item.name">
+						{{item.article.name}} 
+						<v-spacer></v-spacer>
+						<v-btn fab elevation="0" x-small class="mr-2"
+							@click="addSome(item)"
+						>
+							<v-icon>mdi-plus-thick</v-icon>
+						</v-btn>
+						{{item.count}}
+						<v-btn fab elevation="0" x-small class="ml-2"
+							@click="removeSome(item)"
+						>
+							<v-icon>mdi-minus-thick</v-icon>
+						</v-btn>
 					</v-list-item>
 				</v-list-item-group>
 			</v-list>
-			
+			<template v-slot:append>
+				<v-btn block @click="clearCart" color="red" text> CLEAR </v-btn>
+				<v-sheet class="pa-5">
+					Total : {{total}}
+				</v-sheet>
+				<v-dialog
+					v-model="dialog"
+					width="500"
+				>
+					<template v-slot:activator="{ on, attrs }">
+						<v-btn block 
+							text
+							color="primary"
+							v-bind="attrs"
+							v-on="on">Valid</v-btn>
+					</template>
+					<v-card class="overflow-y-auto">
+						<v-card-title>Log user - {{searchUser}}</v-card-title>
+						<v-text-field 
+							class="pa-10" 
+							v-model=searchUser
+							label="User ID" ></v-text-field>
+						
+						<v-btn
+							color="green"
+							text
+							@click="dialog = false"
+							block
+						>
+							Pay
+						</v-btn>
+						<v-btn
+							color="red"
+							text
+							@click="dialog = false"
+							block
+						>
+							Cancel
+						</v-btn>
+					</v-card>
+				</v-dialog>
+				<v-card height="50px" elevation="0" flat><v-card-title></v-card-title></v-card>
+			</template>
 		</v-navigation-drawer>
-		</v-col>
-		<v-col>
-		<Shop
-			v-if="displayShop" 
-			:userCart="userCart"
-			:set="set"
-			v-on:dropArticle="dropArticle"
-			v-on:artClick="artClicked"
-			v-on:checkout="checkout"
-		/>
-		<div v-else>
-			<CreditEdit
-				v-if="selectedPage == 0"
-			/>
-
-			<UserPage 
-				v-else-if="selectedPage == 2"
-				:usersData="usersData"
-				/>
 		</div>
-		</v-col>
-	</v-row>
 </template>
 
 
 <script>
-import Shop from './Shop.vue'
-import UserPage from "./Users.vue"
-import CreditEdit from "./CreditEdit.vue"
 export default{
 	name:"Desktop",
 	props:{
@@ -70,56 +208,102 @@ export default{
 		usersData: Array,
 	},
 	data() {
-		return{
-			displayShop: true,
-			paid: 0,
+		return {
+			show: false,
+			drawer: false,
+			cart: false,
+			dialog: false,
 			userCart: {total:0, cart:[]},
-			
 			selectedSet: 0,
-			selectedPage: null,
+			displaySet: true,
 			menus: [
-				{name:"addCredit",icon: "mdi-cash-plus"},
-				{name:"historic",icon: "mdi-book-arrow-left-outline"},
-				{name:"users",icon: "mdi-account-circle-outline"},
-				{name:"addEvent",icon: "mdi-plus-circle-outline"},
-			]
+				{name:"Users list",icon: "mdi-account-circle-outline"},
+				{name:"Add Credit",icon: "mdi-cash-plus"},
+				//{name:"Add Event",icon: "mdi-plus-circle-outline"},
+			],
+			headers: [
+				{
+					text: 'N° Transaction',
+					align: 'start',
+					sortable: false,
+					value: 'transactionId',
+				},
+				{ text: 'Date', value: 'date' },
+				{ text: 'Transaction', value: 'transactionContent' },
+			],
+			searchInput:"",
+			searchUser:"",
+			computedHeight: window.innerHeight - 100,
+		}
+	},
+	computed: {
+		height () {
+			switch (this.$vuetify.breakpoint.name) {
+			case 'xs': return 220
+			case 'sm': return 400
+			case 'md': return 500
+			case 'lg': return 600
+			case 'xl': return 800
+			default : return 800
+			}
+		},
+		total: function () {
+            let sum = 0;
+            for(let i =0;i < this.userCart.cart.length; i++){
+				if (this.userCart.cart[i].type == "buy")
+					sum = sum + this.userCart.cart[i].article.prix * this.userCart.cart[i].count;
+            }
+            return sum;
+        },
+		set: function(){
+			if (this.displaySet)
+				return this.products[this.selectedSet]
+			else 
+				return null
+		},
+		users: function(){
+			return this.usersData.filter(e => e.username.includes(this.searchInput))
+		},
+		foundUser: function(){
+			let res = this.usersData.filter(e => e.id.includes(this.searchInput))
+			if (res.length == 1)
+				return "User found."
+			else 
+				return "Not found"
 		}
 	},
 	components: {
-		Shop,
-		UserPage,
-		CreditEdit
 	},
-	computed: {
-		set: function(){ 
-			if (this.selectedSet != null){
-				return this.products[this.selectedSet];
-			}
-			else 
-				return this.prevSet
-		}
-	},
+
 	methods: {
-		setMenu(){
-			this.displayShop = false;
-			this.selectedSet = null;
-		},
-		artClicked(spec){
-			
+		addSome(obj){
+            obj.count++;
+        },
+        removeSome(obj){
+            obj.count--;
+            if(obj.count <= 0)
+                this.dropArticle(obj);
+        },
+		dropArticle(obj){
+            const idx = this.userCart.cart.findIndex(item => item.article.name === obj.article.name )
+            this.userCart.cart.splice(idx,1)
+			this.$forceUpdate()
+        },
+		addArt(item){
+			console.log(item.name)
 			for(let i = 0; i < this.userCart.cart.length; i++){
-				if (this.userCart.cart[i].article.name == spec.name){
+				if (this.userCart.cart[i].article.name == item.name){
 					this.userCart.cart[i].count++
 					return true;
 				}
 			}
-			this.userCart.cart.push({type:"buy",article:spec,count: 1})
+			this.userCart.cart.push({type:"buy",article:item,count: 1})
 		},
-		dropArticle(idx){
-			this.userCart.cart.splice(idx,1)
-			this.$forceUpdate()
+		onResize(){ 
+			this.computedHeight = window.innerHeight - 100
 		},
-		checkout(){
-			this.paid = this.userCart.total
+		clearCart(){
+			this.userCart.cart = []
 		}
 	}
 }
---
+++ erp_caisse/src/components/Mobile.vue
@@ -0,0 +1,316 @@
+<template>
+	<div v-resize="onResize">
+
+
+		<v-navigation-drawer
+			v-model="drawer"
+			absolute
+			temporary
+		>
+			<v-list nav class="overflow-y-auto" :max-height=height>
+				<v-list-item-group v-model="selectedSet">
+					<v-list-item
+						v-for="item in products"
+						v-bind:key="item.name"
+						@click="displaySet = true">
+						<v-list-item-icon><v-icon>{{item.icon}}</v-icon></v-list-item-icon>
+						<v-list-item-title>{{item.name}}</v-list-item-title>
+					</v-list-item>
+					<v-divider></v-divider>
+					<v-list-item 
+						v-for="item in menus"
+						v-bind:key="item.name"
+						@click="displaySet = false"
+					>
+						<v-list-item-icon>
+							<v-icon large>{{item.icon}}</v-icon>
+						</v-list-item-icon>
+						<v-list-item-title>{{item.name}}</v-list-item-title>
+					</v-list-item>
+
+				</v-list-item-group>
+			</v-list>
+		</v-navigation-drawer>
+		<v-app-bar-nav-icon 
+			class="pl-5"
+			@click.stop="drawer = !drawer"
+		></v-app-bar-nav-icon>
+
+
+
+<!-- ***************************** SHOP ******************* -->
+		<div v-if="displaySet">
+			<v-list class="overflow-y-auto" :max-height=height >
+				<v-list-item>
+				<v-row class="fill-height pa-5">
+					<v-col cols="4" 
+						class="pa-0"
+						v-for="item in set.articles"
+						v-bind:key="item.name"
+					>
+						<v-card 
+							outlined
+							exact
+							height="100%">
+							<v-card-title> {{item.name}}</v-card-title>
+							<v-card-text class="pl-5 font-light-black"> {{item.prix}}€</v-card-text>
+							<v-card-actions>
+								<v-btn 
+									block
+									@click="addArt(item)"> ADD </v-btn>
+							</v-card-actions>
+						</v-card>
+					</v-col>
+				</v-row>
+				</v-list-item>
+			</v-list>
+			<v-spacer></v-spacer>
+			<v-btn 
+				@click.stop="cart = !cart"
+				block elevation="0"><v-icon>mdi-cart-outline</v-icon></v-btn>
+			<v-sheet elevation="0" align="center"> TOTAL : {{total}}€</v-sheet>
+
+			<v-dialog
+				v-model="dialog"
+				width="500"
+			>
+				<template v-slot:activator="{ on, attrs }">
+					<v-btn block 
+						text
+						color="primary"
+						v-bind="attrs"
+						v-on="on">Valid</v-btn>
+				</template>
+				<v-card class="overflow-y-auto">
+					<v-card-title>Log user - {{searchUser}}</v-card-title>
+					<v-text-field 
+						class="pa-10" 
+						v-model=searchUser
+						label="User ID" ></v-text-field>
+					
+					<v-btn
+						color="green"
+						text
+						@click="dialog = false"
+						block
+					>
+						Pay
+					</v-btn>
+					<v-btn
+						color="red"
+						text
+						@click="dialog = false"
+						block
+					>
+						Cancel
+					</v-btn>
+				</v-card>
+			</v-dialog>
+
+		</div>
+<!-- *************************** USERS LIST ******************* -->
+		<div v-else-if="selectedSet == products.length + 0">  
+			<v-card> 
+				<v-card-title> Users </v-card-title>
+				<v-text-field
+					class="pa-5 pt-0"
+					label="Search username"
+					v-model="searchInput"
+					hide-details="auto"
+				></v-text-field>
+			</v-card>
+			<v-expansion-panels>
+				<v-expansion-panel
+					v-for="item in users"
+					v-bind:key="item.id"
+				>
+					<v-expansion-panel-header>
+						{{item.username}}
+					</v-expansion-panel-header>
+					<v-expansion-panel-content>
+						<span class="font-weight-black"> Name : </span>{{item.username}} <br/>
+						<span class="font-weight-black"> Id : </span>{{item.id}} <br/>
+						<span class="font-weight-black"> Credit : </span>{{item.credit}} <br/>
+						<span class="font-weight-black"> {{item.username}}'s transactions historic : </span><br/>
+						
+						<v-data-table
+							:headers="headers"
+							:items="item.historic">
+						</v-data-table>
+					</v-expansion-panel-content>
+				</v-expansion-panel>
+			</v-expansion-panels>
+		</div>
+
+<!-- *********************** ADD MONEY ****************************** -->
+		<div v-else-if="selectedSet == products.length + 1">
+			<v-card>
+				<v-card-title>Add credit</v-card-title>
+				<v-card-text>
+					<v-text-field 
+						label="User ID"
+					></v-text-field>
+					<v-text-field 
+						label="Amount"
+					></v-text-field>
+					<v-text-field
+						label="Administrator password"
+						:type="show ? 'text' : 'password'"
+						:append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+						@click:append="show = !show"
+					></v-text-field>
+					<v-btn block text color="primary"> Valid </v-btn>
+				</v-card-text>
+			</v-card>
+		</div>
+
+<!-- ************************* CART ************************* -->
+
+		<v-navigation-drawer
+			v-model="cart"
+			absolute
+			right
+			temporary
+		>
+			<v-list class="overflow-y-auto" :max-height=height>
+				<v-list-item-group>
+					<v-list-item 
+						v-for="item in userCart.cart"
+						v-bind:key="item.name">
+						{{item.article.name}} 
+						<v-spacer></v-spacer>
+						<v-btn fab elevation="0" x-small class="mr-2"
+							@click="addSome(item)"
+						>
+							<v-icon>mdi-plus-thick</v-icon>
+						</v-btn>
+						{{item.count}}
+						<v-btn fab elevation="0" x-small class="ml-2"
+							@click="removeSome(item)"
+						>
+							<v-icon>mdi-minus-thick</v-icon>
+						</v-btn>
+					</v-list-item>
+				</v-list-item-group>
+			</v-list>
+			<template v-slot:append>
+				<v-sheet class="pa-5">
+					Total : {{total}}
+				</v-sheet>
+				<v-btn block @click="clearCart" color="red" text> CLEAR </v-btn>
+			</template>
+		</v-navigation-drawer>
+	</div>
+</template>
+
+
+
+<script>
+export default {
+
+	name: 'Mobile',
+	props:{
+			products: Array,
+			usersData: Array,
+	},
+	data() {
+		return {
+			show: false,
+			drawer: false,
+			cart: false,
+			dialog: false,
+			userCart: {total:0, cart:[]},
+			selectedSet: 0,
+			displaySet: true,
+			menus: [
+				{name:"Users list",icon: "mdi-account-circle-outline"},
+				{name:"Add Credit",icon: "mdi-cash-plus"},
+				//{name:"Add Event",icon: "mdi-plus-circle-outline"},
+			],
+			headers: [
+				{
+					text: 'N° Transaction',
+					align: 'start',
+					sortable: false,
+					value: 'transactionId',
+				},
+				{ text: 'Date', value: 'date' },
+				{ text: 'Transaction', value: 'transactionContent' },
+			],
+			searchInput:"",
+			searchUser:"",
+		}
+	},
+	computed: {
+		height () {
+			switch (this.$vuetify.breakpoint.name) {
+			case 'xs': return 220
+			case 'sm': return 400
+			case 'md': return 500
+			case 'lg': return 600
+			case 'xl': return 800
+			default : return 800
+			}
+		},
+		total: function () {
+            let sum = 0;
+            for(let i =0;i < this.userCart.cart.length; i++){
+				if (this.userCart.cart[i].type == "buy")
+					sum = sum + this.userCart.cart[i].article.prix * this.userCart.cart[i].count;
+            }
+            return sum;
+        },
+		set: function(){
+			if (this.displaySet)
+				return this.products[this.selectedSet]
+			else 
+				return null
+		},
+		users: function(){
+			return this.usersData.filter(e => e.username.includes(this.searchInput))
+		},
+		foundUser: function(){
+			let res = this.usersData.filter(e => e.id.includes(this.searchInput))
+			if (res.length == 1)
+				return "User found."
+			else 
+				return "Not found"
+		}
+	},
+	components: {
+	},
+
+	methods: {
+		addSome(obj){
+            obj.count++;
+        },
+        removeSome(obj){
+            obj.count--;
+            if(obj.count <= 0)
+                this.dropArticle(obj);
+        },
+		dropArticle(obj){
+            const idx = this.userCart.cart.findIndex(item => item.article.name === obj.article.name )
+            this.userCart.cart.splice(idx,1)
+			this.$forceUpdate()
+        },
+		addArt(item){
+			console.log(item.name)
+			for(let i = 0; i < this.userCart.cart.length; i++){
+				if (this.userCart.cart[i].article.name == item.name){
+					this.userCart.cart[i].count++
+					return true;
+				}
+			}
+			this.userCart.cart.push({type:"buy",article:item,count: 1})
+		},
+		onResize(){
+			this.windHeight = window.innerHeight - 150
+		},
+		clearCart(){
+			this.userCart.cart = []
+		}
+		
+	}
+};
+</script>
